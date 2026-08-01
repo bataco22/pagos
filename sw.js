4b8bc38ff68b8322f0e0a726c9b24107ebb2d464
bataco22 <axeldeanda@hotmail.com>
2026-08-01 13:04:11 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='pagos-v10-gastos';
+const CACHE='pagos-v11-resumen-prioridades';
 const ASSETS=['./','index.html','manifest.webmanifest','icon-192.png','icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
